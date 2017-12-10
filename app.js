ffea7a11c5ec3257e496327339e3e85c24b495d8
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-10 20:00:37 -0200

    Task1

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ new Vue({
         el: '#exercise3',
         data: {
             value: 0
+        },
+        computed: {
+        	result: function() {
+        		return this.value < 37 ? "Not yet there" : "Done";
+        	}
         }
     });
