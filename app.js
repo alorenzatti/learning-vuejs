18a955771677ff1b3ab21fec4cb046478f030539
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-10 18:53:11 -0200

    Task 1

---
+++ app.js
@@ -0,0 +1,11 @@
+new Vue({
+        el: '#exercise2',
+        data: {
+            value: ''
+        },
+        methods: {
+        	alert : function() {
+        		alert('You are being alerted!');
+        	}
+        }
+    });
