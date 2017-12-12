6f4a6193fb96115297a7a51379d0ff012740aafa
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-11 22:57:15 -0200

    Task 1

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 new Vue({
   el: '#exercise4',
   data: {
-
+    effectClassState: false
+  },
+  computed : {
+    effectClass: function() {
+        return  this.effectClassState ? 'highlight' : 'shrink';
+    }
   },
   methods: {
     startEffect: function() {
-    
+        var self = this;
+        setInterval(function() {
+            self.effectClassState = !self.effectClassState;
+        },2000);
+    },
+    startProgress: function() {
+
     }
   }
 });
