ce16d0b601a544b64f78318e46e964524aec20f2
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-25 15:27:33 -0200

    Task 2

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 new Vue({
   el: '#exercise4',
   data: {
-    effectClassState: false
+    effectClassState: false,
+    colorClasses: ['color-red','color-green','color-blue'] ,
+    currentColor: -1,
+    sizeClasses : ['size-50','size-100','size-150'],
+    currentSize: -1
+
   },
   computed : {
     effectClass: function() {
         return  this.effectClassState ? 'highlight' : 'shrink';
+    },
+
+    colorClass : function() {
+
+      return this.colorClasses[this.currentColor];
+    },
+
+
+    sizeClass : function() {
+
+      return this.sizeClasses[this.currentSize];
     }
   },
   methods: {
@@ -14,7 +30,21 @@ new Vue({
         setInterval(function() {
             self.effectClassState = !self.effectClassState;
         },2000);
+
+        setInterval(this.setColor, 1500);
+        setInterval(this.setSize, 2500);
     },
+
+    setColor : function() {
+
+      this.currentColor = this.currentColor < this.colorClasses.length - 1 ? this.currentColor + 1 : 0;
+    },
+
+    setSize : function() {
+      
+      this.currentSize = this.currentSize < this.sizeClasses.length - 1 ? this.currentSize + 1 : 0;
+    },
+
     startProgress: function() {
 
     }
