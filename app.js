40d9906cb6727f1010eb61b3ff45869b53f95ce0
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-25 18:11:16 -0200

    Task 4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
   data: { 
     showParagraphs: false,
     elements: ['TV', 'Bed', 'Computer', 'Keyboard'],
-    libraryEntry: {title: 'Lord of the Rings', author: 'J.R.R. Tolkiens', books: '3'}
+    libraryEntry: {title: 'Lord of the Rings', author: 'J.R.R. Tolkiens', books: '3'},
+    object: {name: 'TESTOBJECT', data: [1.67, 1.33, 0.98, 2.21]}
   }     
 }); 
