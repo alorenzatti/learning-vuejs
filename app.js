0acc91854c921895204904451bd10f4f1329b2fe
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-25 17:20:36 -0200

    Basic structure

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ new Vue({
 
   el: '#app', 
   data: { 
-    show: false
+    ingredients: ['meat', 'fruit', 'cookies'],
+    people: [
+      {name: 'Max', age: 27, color:'red'},
+      {name: 'Anna', age: 'unknown', color:'blue'}
+    ]
   }     
 }); 
