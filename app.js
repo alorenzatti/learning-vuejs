9c228d930a66cbc4b52f05b7b94fc83c639c71b5
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-30 14:44:17 -0200

    Included JS functions

--- app.js
+++ app.js
@@ -1,7 +1,162 @@
+const DANGER_HEALTH = 25
+const DEATH_HEALTH = 10
+
 new Vue({ 
 
   el: '#app', 
   data: { 
-    title: 'Hello World!' 
-  }     
+    states : ['off', 'game-over'],
+    currentState : 0,
+    playerHealth : 0,
+    monsterHealth : 0,
+    logs: [],
+    ATTACK_POWER : 0,
+    SPECIAL_ATTACK_POWER : 0,
+    HEAL_POWER: 0
+  },
+
+  computed : {
+
+    gameState : function() {
+
+      return this.states[this.currentState]
+    },
+    
+    playerHealthBar : function() {
+
+      return this.getProgressBarStyle(this.playerHealth)
+    },
+
+    monsterHealthBar : function() {
+
+      return this.getProgressBarStyle(this.monsterHealth)
+    }
+  },
+
+  methods : {
+
+    start: function() {
+
+      this.ATTACK_POWER = 10
+      this.SPECIAL_ATTACK_POWER = this.ATTACK_POWER * 2
+
+      this.HEAL_POWER = this.SPECIAL_ATTACK_POWER / 1.5
+
+      this.logs = []
+
+      this.playerHealth = 100
+      this.monsterHealth = 100
+      this.currentState++
+    },
+
+    attack : function(attackPower) {
+      
+      this.playerAttack(attackPower)
+      this.monsterAttack(this.ATTACK_POWER)
+
+      this.checkGameStatus()
+    },
+
+    playerAttack : function(attackPower) {
+
+      var playerAttack = this.calculateAttack(attackPower, this.playerHealth)
+
+      this.monsterHealth -= playerAttack
+      
+      this.playerHealth = this.playerHealth < 0 ? 0 : this.playerHealth
+      
+      this.log("PLAYER", "HITS", "MONSTER", playerAttack)
+    },
+
+    monsterAttack : function(attackPower) {
+
+      var monsterAttack = this.calculateAttack(attackPower, this.monsterHealth)
+
+      this.playerHealth -= monsterAttack
+      
+      this.monsterHealth = this.monsterHealth < 0 ? 0 : this.monsterHealth
+
+      this.log("MONSTER", "HITS", "PLAYER", monsterAttack)
+    },
+
+    specialAttack : function() {
+
+      this.attack(this.SPECIAL_ATTACK_POWER)
+    },
+
+    heal : function() {
+
+      var playerHeal = this.calculateHeal()
+      this.playerHealth += playerHeal
+
+      if(this.playerHealth > 100) {
+
+        this.playerHealth = 100
+
+      } else {
+
+        this.log("PLAYER", "HEALS", "HIMSELF", playerHeal)
+        this.monsterAttack(this.ATTACK_POWER)
+      }
+
+      this.checkGameStatus()
+    },
+
+    giveUp : function() {
+
+      this.currentState = 0
+      this.playerHealth = 0
+      this.monsterHealth = 0
+
+      this.logs = []
+    },
+
+    checkGameStatus : function() {
+
+      var playerLooses = this.playerHealth <= 0
+      var monsterLooses = this.monsterHealth <= 0
+
+      if(playerLooses || monsterLooses) {
+
+        var winner = playerLooses ? "MONSTER" : "PLAYER"
+
+        if(window.confirm(winner + " wins! Play again?")) {
+
+          this.start()
+
+        } else {
+
+          this.giveUp()
+        }
+      }
+    },
+
+    calculateAttack : function(attackPower, health) {
+
+      return health > 0 ? Math.round(attackPower * Math.random()) : 0 
+    },
+    
+    calculateHeal : function() {
+
+      return Math.round(this.HEAL_POWER * Math.random())
+    },
+
+    getProgressBarStyle : function(health) {
+
+      var style = health > DANGER_HEALTH ?
+        'width: ' + health + '%; background-color: green;' :
+        'width: ' + health + '%; background-color: red;'
+
+      style = health > DEATH_HEALTH ?
+        style :
+        'width: ' + health + '%; background-color: purple;'
+
+      return style
+    },
+
+    log : function(attacker, action, attacked, amount) {
+
+      this.logs.splice(0, 0, attacker + " " + action + " " + attacked + " FOR " + amount)
+    }
+  }
 }); 
