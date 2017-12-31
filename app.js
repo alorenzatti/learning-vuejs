52a491f18fadc7486db3abd71e93da21dd390e81
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-31 16:39:29 -0200

    Vue lifecycle callbacks

--- app.js
+++ app.js
@@ -3,5 +3,44 @@ new Vue({
   el: '#app', 
   data: { 
     title: 'Hello World!' 
-  }     
+  },
+  
+  beforeCreate: function() {
+    console.log('beforeCreate()')
+  },
+
+  created: function() {
+    console.log('created()')
+  },
+
+  beforeMount: function() {
+    console.log('beforeMount()')
+  },
+
+  mounted: function() {
+    console.log('mounted()')
+  },
+
+  beforeUpdate: function() {
+    console.log('beforeUpdated()')
+  },
+
+  updated: function() {
+    console.log('updated()')
+  },
+
+  beforeDestroy: function() {
+    console.log('beforeDestroy()')
+  },
+
+  destroyed:  function() {
+    console.log('destroyed()')
+  },
+
+  methods : {
+
+    destroy: function() {
+      this.$destroy()
+    }
+  }
 }); 
