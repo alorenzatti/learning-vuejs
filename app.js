e626488d75c3df782571e8900d116ddca32bc2da
Alexandre Lorenzatti <alorenza@gmail.com>
2017-12-31 14:49:19 -0200

    Using ref keyword

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var vm1 = new Vue({
     show: function() {
       this.showParagraph = true;
       this.updateTitle('The VueJS Instance (Updated)');
+      this.$refs.myButton.innerText = 'Test'
     },
     updateTitle: function(title) {
       this.title = title;
@@ -25,7 +26,9 @@ var vm1 = new Vue({
   }
 });
 
+
 vm1.title = "Changed!"
+
 
 var vm2 = new Vue({
   el: '#app2',
