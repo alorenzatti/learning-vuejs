f1bb7299c65dff3e18bc9e4b641d9cc58faef019
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-02 22:16:44 -0200

    Working with props

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,19 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ name }} </p>
+        <p>User Name: {{ switchName() }} </p>
     </div>
 </template>
 
 <script>
 	export default {
-		props: ['name']
+		props: ['name'],
+		methods : {
+			switchName : function() {
+
+				return this.name.split("").reverse().join("")
+			}
+		}
 	}
 </script>
 
