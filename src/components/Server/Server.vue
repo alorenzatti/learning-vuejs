dbea7caf4fd4423529cda5ab1b791d755891093b
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-04 00:05:36 -0200

    Included the Server component

---
+++ src/components/Server/Server.vue
@@ -0,0 +1,26 @@
+<template>
+	<li
+		class="list-group-item"
+		@click="serverSelected"
+		style="cursor: pointer">
+
+		<p>Server {{ server.id }} status is {{ server.status }}</p>
+	</li>
+</template>
+
+<script>
+	import {serverBus} from '../../main'
+
+	export default {
+		
+		props : ['server'],
+
+		methods : {
+
+			serverSelected : function() {
+				
+				serverBus.$emit('serverSelected', this.server)
+			}
+		}
+	}
+</script>
