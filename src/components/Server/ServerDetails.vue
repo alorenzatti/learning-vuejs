232399ae843910c288be31e061c5fdcd3b543631
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-04 00:06:57 -0200

    Improved tthe Server Details component

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,8 +1,37 @@
 <template>
 	<div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+		<p v-if="!server">Please select a server.</p>
+		<span v-else>
+        	<p>Server {{ server.id }} current status is: {{ server.status }}</p>
+         	<button @click="refreshStatus">Refresh status</button>
+		</span>
     </div>
 </template>
 
 <script>
+	import {serverBus} from '../../main'
+
+	export default {
+
+		data : function() {
+
+			return {
+				server : null,
+				statuses : ['Normal', 'Critical', 'Unknown', 'Off-line']
+			}
+		},
+
+		created : function() {
+			serverBus.$on('serverSelected', (server) => {
+				this.server = server
+			})
+		},
+
+		methods : {
+
+			refreshStatus : function() {
+				this.server.status = this.statuses[Math.floor(Math.random() * this.statuses.length)]
+			}
+		}
+	}
 </script>
