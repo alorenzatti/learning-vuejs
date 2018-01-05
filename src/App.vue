2d1ec5fa8880189d7ebaf643271f149d04057408
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-04 22:08:28 -0200

    Content compiled and styled

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
               <app-quote>
-                <h2>The Quote</h2>
+                <h2>{{ quoteTitle }}</h2>
                 <p>A random quote!</p>
               </app-quote>
             </div>
@@ -16,6 +16,12 @@
   import Quote from './components/Quote.vue'
 
   export default {
+    
+    data : function() {
+      return {
+        quoteTitle : 'The Quote'
+      }
+    },
 
     components : {
       appQuote : Quote
@@ -23,5 +29,8 @@
   }
 </script>
 
-<style>
+<style scope>
+  h2 {
+    color: blue;
+  }
 </style>
--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -16,4 +16,8 @@
 		margin: 30px auto;
 		text-align: center;
 	}
+
+	h2 {
+		color: red;
+	}
 </style>
