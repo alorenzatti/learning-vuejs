8ac06601dddb92f73aa769a7eb4218d64e99d6f0
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-06 00:14:39 -0200

    Basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Components</h1>
-
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary">Load Blue Template</button>
+                <button class="btn btn-success">Load Green Template</button>
+                <button class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
     export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
     }
 </script>
 
 <style>
-
 </style>
