8c1955ffff7e2163f97fd8e97f1ef403cb899628
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-07 20:21:36 -0200

    Included Quote component

---
+++ src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+	<div @click="deleteQuote">
+		<p>{{ quote.text }}</p>
+	</div>
+</template>
+
+<script>
+	import {quoteBus} from '../main.js'
+	export default {
+		props : ['quote'],
+
+		methods : {
+			deleteQuote : function() {
+				quoteBus.$emit('deleteQuote', this.quote.index)
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	div {
+		padding: 5px;
+		margin: 5px;
+		border: 1px solid black;
+		border-radius: 2px;
+		font-size: 28px;
+		font-family: Arizonia;
+		color: #AAA;
+	}
+</style>
