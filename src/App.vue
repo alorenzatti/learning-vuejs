0f1c3566171f08f0a6ba071107bafbef5b1b76ca
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-06 23:12:56 -0200

    Included header component to app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
     <div class="container">
-    
+        <appHeader
+          :progressStatus="quotes.length"
+          :max="maxQuotes"><h1 slot="headerMessage">Quotes Added</h1></appHeader>
+        NewQuote
+        AllQuotes
+        SingleQuote
     </div>
 </template>
 
 <script>
+    import Header from './components/Header.vue'
+
     export default {
         
+        created : function() {
+          
+        },
+
+        data : function() {
+          return {
+            maxQuotes : 10,
+            quotes : []
+          }
+        },
+
+        components : {
+          appHeader : Header
+        }
     }
 </script>
 
-<style>
+<style scoped>
+  .container {
+    padding: 25px;
+  }
 </style>
