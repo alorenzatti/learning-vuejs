a9d83abef4acbbd108041f0c866e073fd9460cd2
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-06 23:12:37 -0200

    Created header component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<slot name="headerMessage"></slot>
+		<appProgressBar
+        	:progressStatus="progressStatus"
+        	:max="max"
+    	>
+    	</appProgressBar>
+	</div>
+</template>
+
+<script>
+	import ProgressBar from './ProgressBar.vue'
+
+	export default {
+		
+		props : ['progressStatus', 'max'],
+
+		components : {
+
+          appProgressBar : ProgressBar
+        }
+	}
+</script>
+
+<style>
+</style>
