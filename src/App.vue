96d28c7b9d678f5c0eb9142364fcbb2bd61c4376
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-14 16:52:42 -0200

    Basic form

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,42 @@
         <form>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+                  <!-- Exercise 1 -->
+                  <!-- Create a Signup Form where you retrieve the following Information -->
+                  <!-- Full Name (First Name + Last Name) -->
+                  <!-- Mail -->
+                  <!-- Password -->
+                  <!-- Store Data? Yes/No -->
+                  <form>
+                    <label>First name:
+                      <input type="text" name="firstName" v-model.lazy="firstName" />
+                    </label>
+                    <label>Last name:
+                      <input type="text" name="lastName" v-model.lazy="lastName" />
+                    </label>
+                    <label>Mail:
+                      <input type="text" name="mail" v-model.lazy="mail" />
+                    </label>
+                    <label>Password:
+                      <input type="password" name="password" v-model.lazy="password" />
+                    </label>
+                    <label for="yes">
+                      Store data? 
+                      <input 
+                      type="radio" 
+                      id="yes" 
+                      value="Yes" 
+                      v-model="storeInfo"> Yes
+                    </label>
+                    <label for="no"> 
+                      <input 
+                      type="radio" 
+                      id="no" 
+                      value="No" 
+                      v-model="storeInfo"> No
+                    </label>
+                  </form>
+                    
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
@@ -28,10 +58,10 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }} </p>
+                        <p>Mail: {{ mail }} </p>
+                        <p>Password: {{ password }} </p>
+                        <p>Store in Database?: {{ storeInfo }}</p>
                     </div>
                 </div>
             </div>
@@ -41,6 +71,23 @@
 
 <script>
     export default {
+      data : function() {
+        return {
+          firstName : '',
+          lastName : '',
+          mail : '',
+          password : '',
+          storeInfo : 'No'
+        }
+      },
+
+      computed : {
+
+        fullName : function() {
+
+          return this.firstName + " " + this.lastName
+        }
+      }
     }
 </script>
 
