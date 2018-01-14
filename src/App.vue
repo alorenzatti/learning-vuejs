bf33c51188643d77fd546f4ebc3851789570b55a
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-14 21:42:17 -0200

    Local filter

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
               <h1>Filters</h1>
+              <p>{{ text | toUppercase }}</p>
             </div>
         </div>
     </div>
@@ -10,6 +11,18 @@
 
 <script>
     export default {
+      data : function() {
+        return {
+          text : 'Hello there!'
+        }
+      },
+
+      filters : {
+        toUppercase : function(value) {
+
+          return value.toUpperCase()
+        }
+      }
     }
 </script>
 
