7f6ff95ce62c294cdaf100af68ba3aa3bed52a8a
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-14 21:38:29 -0200

    Basic structure

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-
+              <h1>Filters</h1>
             </div>
         </div>
     </div>
