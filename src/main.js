cdaf7b40ac8ea397669a0866581f506a7dbbbd8f
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-18 22:59:05 -0200

    Global mixin

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ Vue.filter('toLowercase', function(value) {
   return value.toLowerCase()
 })
 
+Vue.mixin({
+  created : function() {
+    console.log("Global mixin created Hook")
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
