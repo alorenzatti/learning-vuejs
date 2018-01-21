42906fea9c2595985780ec7daa66b813b00f2232
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-21 20:08:55 -0200

    Synchronizing animation and transition

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
               <transition name="fade">
                 <div class="alert alert-info" v-if="show">This is some info</div>
               </transition>
-              <transition name="slide">
+              <transition name="slide" type="animation">
                 <div class="alert alert-info" v-if="show">This is some info</div>
               </transition>
             </div>
@@ -46,11 +46,12 @@
   }
 
   .slide-enter {
-
+    opacity: 0;
   }
 
   .slide-enter-active {
     animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
   }
 
   .slide-leave {
@@ -59,6 +60,8 @@
 
   .slide-leave-active {
     animation: slide-out 1s ease-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
   }
 
   @keyframes slide-in {
