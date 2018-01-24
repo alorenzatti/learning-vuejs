090d76f4bfd094b256e33579d6db850587405920
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-23 22:06:14 -0200

    Included Answer component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <app-answer></app-answer>
         <app-question></app-question>
       </div>
     </div>
@@ -9,10 +10,12 @@
 </template>
 
 <script>
+  import Answer from "./components/Answer.vue"
   import Question from "./components/Question.vue"
 
   export default {
     components : {
+      "app-answer" : Answer,
       "app-question" : Question
     }
   }
