7985aa8f4f51724d11d971b0cb1d78cd01b5e92e
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-23 22:03:49 -0200

    Included Question component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <app-question></app-question>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
+  import Question from "./components/Question.vue"
+
+  export default {
+    components : {
+      "app-question" : Question
     }
+  }
 </script>
 
 <style>
