98cb1698d263bd6375019c060238c742181862b8
Alexandre Lorenzatti <alorenza@gmail.com>
2018-01-24 00:10:21 -0200

    Improved Answer component

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="alert alert-success text-center">
-    <h1>That's Correct!</h1>
-    <hr>
-    <button class="btn btn-primary"></button>
+  <div
+    class="alert text-center"
+    :class="alertClass">
+    <div v-if="correct">
+      <h1>That's Correct!</h1>
+      <hr>
+      <button class="btn btn-primary" @click="onNextQuestion">Next question</button>
+    </div>
+    <div v-else>
+      <h1>Try again!</h1>
+    </div>
+
   </div>
 </template>
 <style>
@@ -11,9 +19,18 @@
 <script>
 
   export default{
+    props : ["correct"],
+
+    computed : {
+      alertClass : function() {
+
+        return this.correct ? "alert-success" : "alert-danger"
+      }
+    },
+
     methods: {
       onNextQuestion() {
-        this.$emit('confirmed');
+        this.$emit('next-question');
       }
     }
   }
