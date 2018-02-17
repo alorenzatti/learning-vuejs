63c7726a1aa780a477637184bf0360629d12074e
Alexandre Lorenzatti <alorenza@gmail.com>
2018-02-17 00:14:40 -0200

    Included form

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
               <h1>HTTP</h1>
+              <div class="form-group">
+                <label>Username</label>
+                <input class="form-control" type="text" v-model="user.username">
+              </div>
+              <div class="form-group">
+                <label>Email</label>
+                <input class="form-control" type="text" v-model="user.email">
+              </div>
+              <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,21 @@
 
 <script>
     export default {
+
+      data : function() {
+        return {
+          user : {
+            username : "",
+            email : ""
+          }
+        }
+      },
+
+      methods : {
+        submit : function() {
+          console.log(this.user)
+        }
+      }
     }
 </script>
 
