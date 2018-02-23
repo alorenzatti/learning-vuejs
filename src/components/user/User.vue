4c6e90827190549a7e67298f7f29cc7906018403
Alexandre Lorenzatti <alorenza@gmail.com>
2018-02-22 23:20:29 -0300

    Children routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,26 +2,15 @@
     <div>
       <h1>The User Page</h1>
       <hr>
-      <p>Loaded id: {{ id }}</p>
       <button @click="navigateToHome">Go home!</button>
+      <hr>
+      <router-view></router-view>
     </div>
 
 </template>
 
 <script>
   export default {
-    data : function() {
-      return {
-        id : this.$route.params.id
-      }
-    },
-
-    watch : {
-      "$route" : function(to, from) {
-        this.id = to.params.id
-      }
-    },
-    
     methods : {
       navigateToHome : function() {
         this.$router.push("/")
