e235ddf2b5fe0257a95f9e70e2d995526e9ea892
Alexandre Lorenzatti <alorenza@gmail.com>
2018-02-22 22:45:06 -0300

    Routing from code

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,18 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+      <h1>The User Page</h1>
+      <hr>
+      <button @click="navigateToHome">Go home!</button>
+    </div>
 
 </template>
+
+<script>
+  export default {
+    methods : {
+      navigateToHome : function() {
+        this.$router.push("/")
+      }
+    }
+  }
+</script>
