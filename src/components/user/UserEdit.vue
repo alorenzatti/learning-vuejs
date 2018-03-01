fa548bef4e8bd3f925bcbce38ebb8d80bf9b46ed
Alexandre Lorenzatti <alorenza@gmail.com>
2018-02-28 23:10:01 -0300

    Before leave guard

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,8 +4,30 @@
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Query: {{ $route.query.q }}</p>
     <hr>
-    <button class="btn btn-primary">Confirm?</button>
+    <button class="btn btn-primary" @click="confirmed = true">Confirm?</button>
     <div style= "height: 1700px;"></div>
     <p id="data">Some extra</p>
   </div>
 </template>
+
+<script>
+  export default {
+    data : function() {
+      return {
+        confirmed : false
+      }
+    },
+
+    beforeRouteLeave : function(to, from, next) {
+      if(this.confirmed) {
+        next()
+      } else {
+        if(confirm("Are you sure?")) {
+          next()
+        } else {
+          next(false)
+        }
+      }
+    }
+  }
+</script>
